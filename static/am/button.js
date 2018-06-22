4a8f9a861875f182a3cca3110c4e7175e6f630f2
yuyan <yu-yan-good@163.com>
2018-06-22 14:59:51 +0800

    优化样式，增加按钮颜色

---
+++ static/am/button.js
@@ -0,0 +1,9 @@
+$('body').on('mouseover', '#addButton, #moreInfo, #edit, #search, #backButton, #editButton', function(){
+    $(this).css({"background-color": "#15806B"});
+}).on('mouseout', '#addButton, #moreInfo, #edit, #search, #backButton, #editButton', function(){
+    $(this).css({"background-color": "#18a689"});
+}).on('mouseover', '#delButton, #del', function () {
+    $(this).css({"background-color": "#BB4352"});
+}).on('mouseout', '#delButton, #del', function () {
+    $(this).css({"background-color": "#ed5565"});
+});
